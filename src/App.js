aa5fda756a8594b353a66eb769be9ec9510f8034
LibeMan <robbe.95@hotmail.com>
2021-08-06 11:43:19 +0300

    2.6-2.10

--- src/App.js
+++ src/App.js
@@ -1,64 +1,60 @@
-import React from 'react'
-import Course from './components/Course'
+import React, { useState } from 'react'
+import Person from './components/Person'
+import PersonAll from './components/PersonAll'
+import Form from './components/Form'
+import Filter from './components/Filter'
 
-const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+const App = (props) => {
+  const [ persons, setPersons ] = useState(props.persons) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ showAll, setShowAll ] = useState('')
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const handleShowChange = (event) => {
+    console.log(event.target.value)
+    setShowAll(event.target.value)
+  }
+
+  
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber
     }
-  ]
+    persons.forEach(function(item){
+      if (newName === item.name || newNumber === item.number){
+        window.alert('Woopsidooo!! That name or number is already added to the phonebook')
+      } else {
+        setPersons(persons.concat(personObject))
+        setNewName('')
+        setNewNumber('')
+      }
+    })
+    
+  }
 
   return (
-
     <div>
-      {courses.map(note => 
-        <Course key={note.id} course={note} />
-      )}
+      <h2>Phonebook</h2>
+      <Filter showAll={showAll} handleShowChange={handleShowChange} />
+      <Form newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} addPerson={addPerson}/>
+      <h2>Numbers</h2>
+      <ul>
+        <PersonAll persons={persons} showAll={showAll}/>
+      </ul>
     </div>
-    
-    
-    
-  ) 
-  
+  )
 }
 
 export default App
---
+++ src/components/Filter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Filter = ({ showAll, handleShowChange }) => {
+  return (
+    <div>filter shown with <input
+        value={showAll}
+        onChange={handleShowChange}
+        />
+    </div>
+  )
+}
+
+export default Filter
